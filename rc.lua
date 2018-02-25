a9b620047a8a2d44d32946c7ec7b414bdfc8e279
Erik Dubois <erik.dubois@gmail.com>
2018-02-25 11:33:16 +0100

    archmerge replaced

--- rc.lua
+++ rc.lua
@@ -397,7 +397,7 @@ globalkeys = my_table.join(
         {description = "Xfce appfinder", group = "altkey"}),
 
     -- screenshots
-    awful.key({ }, "Print", function () awful.util.spawn("scrot 'ArchMergeD-%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f $$(xdg-user-dir PICTURES)'") end,
+    awful.key({ }, "Print", function () awful.util.spawn("scrot 'ArcoLinuxD-%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f $$(xdg-user-dir PICTURES)'") end,
         {description = "Scrot", group = "screenshots"}),
     awful.key({ modkey1           }, "Print", function () awful.util.spawn( "xfce4-screenshooter" ) end,
         {description = "Xfce screenshot", group = "screenshots"}),
