e5f8d6258f71cde46a6e19f530f4df5d004b2246
Nick Petrov <nickpetrov75@gmail.com>
2018-05-04 07:25:41 -0700

    pywal added

--- rc.lua
+++ rc.lua
@@ -380,7 +380,7 @@ globalkeys = my_table.join(
     awful.key({ altkey, "Shift"   }, "n", function () awful.spawn.with_shell( "variety -n  && wal -i $(cat $HOME/.config/variety/wallpaper/wallpaper.jpg.txt)&" ) end,
         {description = "Pywal Wallpaper next", group = "altkey"}),
     awful.key({ altkey, "Shift"   }, "u", function () awful.spawn.with_shell( "wal -i $(cat $HOME/.config/variety/wallpaper/wallpaper.jpg.txt)&" ) end,
-        {description = "Pywal Wallpaper next", group = "altkey"}),
+        {description = "Pywal Wallpaper update", group = "altkey"}),
     awful.key({ altkey, "Shift"   }, "p", function () awful.spawn.with_shell( "variety -p  && wal -i $(cat $HOME/.config/variety/wallpaper/wallpaper.jpg.txt)&" ) end,
         {description = "Pywal Wallpaper previous", group = "altkey"}),
     awful.key({ altkey }, "t", function () awful.util.spawn( "variety -t" ) end,
