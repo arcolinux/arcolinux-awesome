446a499c06918e6a9ac7cda0aba61942c806df58
deadsidestar <sxycoup75@protonmail.com>
2019-05-06 01:09:43 -0700

    added terminal border on single window

--- etc/skel/.config/awesome/rc.lua
+++ etc/skel/.config/awesome/rc.lua
@@ -273,7 +273,7 @@ screen.connect_signal("arrange", function (s)
     local only_one = #s.tiled_clients == 1
     for _, c in pairs(s.clients) do
         if only_one and not c.floating or c.maximized then
-            c.border_width = 0
+            c.border_width = 2
         else
             c.border_width = beautiful.border_width
         end
