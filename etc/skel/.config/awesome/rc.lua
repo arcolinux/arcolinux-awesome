c71bb8a153665a5d4f9bd49baebaba8da35b920c
deadsidestar <sxycoup75@protonmail.com>
2019-05-07 08:15:57 -0700

    dmenu keybinding conflict fixed

--- etc/skel/.config/awesome/rc.lua
+++ etc/skel/.config/awesome/rc.lua
@@ -602,7 +602,7 @@ globalkeys = my_table.join(
               {description = "move tag to the left", group = "tag"}),
     awful.key({ modkey, "Shift" }, "Right", function () lain.util.move_tag(1) end,
               {description = "move tag to the right", group = "tag"}),
-    awful.key({ modkey, "Shift" }, "d", function () lain.util.delete_tag() end,
+    awful.key({ modkey, "Shift" }, "y", function () lain.util.delete_tag() end,
               {description = "delete tag", group = "tag"}),
 
     -- Standard program
