d008ae7ff5b27a172ffcc813ab97c596d9bac93f
Erik Dubois <erik.dubois@gmail.com>
2019-05-09 22:29:28 +0200

    raise=true

--- etc/skel/.config/awesome/rc.lua
+++ etc/skel/.config/awesome/rc.lua
@@ -1112,7 +1112,7 @@ end)
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = true})
+    c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
