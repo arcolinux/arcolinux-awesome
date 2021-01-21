d52ef22ae03c56b2e1dc95219dad2beea2a05294
Erik Dubois <erik.dubois@gmail.com>
2021-01-21 06:44:23 +0100

    tip from zd4y - autostart without "

--- etc/skel/.config/awesome/autostart.sh
+++ etc/skel/.config/awesome/autostart.sh
@@ -6,28 +6,28 @@ function run {
     $@&
   fi
 }
-run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
-#run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
-#run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
-run "nm-applet"
-#run "caffeine"
-run "pamac-tray"
-run "variety"
-run "xfce4-power-manager"
-run "blueberry-tray"
-run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-run "numlockx on"
-run "volumeicon"
-#run "nitrogen --restore"
-run "conky -c $HOME/.config/awesome/system-overview"
+run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+#run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+run nm-applet
+#run caffeine
+run pamac-tray
+run variety
+run xfce4-power-manager
+run blueberry-tray
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run numlockx on
+run volumeicon
+#run nitrogen --restore
+run conky -c $HOME/.config/awesome/system-overview
 #you can set wallpapers in themes as well
 feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 #run applications from startup
-#run "firefox"
-#run "atom"
-#run "dropbox"
-#run "insync start"
-#run "spotify"
-#run "ckb-next -b"
-#run "discord"
-#run "telegram-desktop"
+#run firefox
+#run atom
+#run dropbox
+#run insync start
+#run spotify
+#run ckb-next -b
+#run discord
+#run telegram-desktop
