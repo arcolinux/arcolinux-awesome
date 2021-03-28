3d6c73903c2ac962df2b8111025f29ae3d9046e7
Erik Dubois <erik.dubois@gmail.com>
2021-03-28 13:28:00 +0200

    xlunch hidden

--- etc/skel/.config/awesome/rc.lua
+++ etc/skel/.config/awesome/rc.lua
@@ -444,6 +444,8 @@ globalkeys = my_table.join(
         {description = "Gmrun", group = "altkey"}),
     awful.key({ altkey }, "F3", function () awful.util.spawn( "xfce4-appfinder" ) end,
         {description = "Xfce appfinder", group = "altkey"}),
+    -- awful.key({ altkey }, "F5", function () awful.spawn.with_shell( "xlunch --config ~/.config/xlunch/default.conf --input ~/.config/xlunch/entries.dsv" ) end,
+    --    {description = "Xlunch app launcher", group = "altkey"}),
 
     -- screenshots
     awful.key({ }, "Print", function () awful.util.spawn("scrot 'ArcoLinux-%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f $$(xdg-user-dir PICTURES)'") end,
