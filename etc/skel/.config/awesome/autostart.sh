12a05152f486456b44ae0b7df601c9a277a8e15b
Erik Dubois <erik.dubois@gmail.com>
2021-05-08 20:27:30 +0200

    autorandr

--- etc/skel/.config/awesome/autostart.sh
+++ etc/skel/.config/awesome/autostart.sh
@@ -9,6 +9,7 @@ function run {
 run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 #run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+autorandr horizontal
 run nm-applet
 #run caffeine
 run pamac-tray
