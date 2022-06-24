d50870ffa95113e5e83c533e3cc37f572be50b51
arcolinuxz <arcolinuxinfo@gmail.com>
2022-06-24 10:15:58 +0200

    wallpaper

--- etc/skel/.config/awesome/autostart.sh
+++ etc/skel/.config/awesome/autostart.sh
@@ -22,8 +22,8 @@ run volumeicon
 #run nitrogen --restore
 run conky -c $HOME/.config/awesome/system-overview
 #you can set wallpapers in themes as well
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
+feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 #run applications from startup
 #run firefox
 #run atom
